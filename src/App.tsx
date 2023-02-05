6acff010bb5243db1fc88b42a6a4b517ac7cbcc6
Secret240rieur <jawharbeliber@gmail.com>
2023-02-05 18:25:19 -0500

    build commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import gameOver from './photo/game_over_neon_lights_hd_game_over.jpg';
 import { FaTimes } from "react-icons/fa";
 import { useAuthUser, useAuthSignOut } from "@react-query-firebase/auth";
-import { collection, DocumentData, getFirestore, query, where, doc, updateDoc } from "firebase/firestore";
-import { useFirestoreCollectionMutation, useFirestoreQuery } from "@react-query-firebase/firestore";
+import { collection, DocumentData, getFirestore, query, doc, onSnapshot, orderBy } from "firebase/firestore";
+import { useFirestoreDocument, useFirestoreDocumentMutation } from "@react-query-firebase/firestore";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { SignUpGoogle } from "./signUpGoogle";
+import classNames from "classnames";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -44,43 +45,45 @@ function App() {
   const [inf, setInf] = useState(false);
   const [closeBtnVisible, setCloseBtnVisible] = useState(true);
   const [transition, setTransition] = useState('translate-x-full');
-
+  const [started, setStarted] = useState(false);
 
   const user = useAuthUser(["user"], auth);
-  console.log(user.data?.uid);
   //upload
   const refUpload = collection(firestore, "leaderboard");
-  const highScoreUpload = useFirestoreCollectionMutation(refUpload);
+  let highscoreRef = doc(refUpload, user.data ? user.data?.uid : 'undefined');
+  const highScoreUpload = useFirestoreDocumentMutation(highscoreRef);
 
 
 
-  const refDownload = query(collection(firestore, "leaderboard"));
-  const downloadQuery = useFirestoreQuery(['leaderboard'], refDownload);
+  const refDownload = query(refUpload, orderBy("highScore", "desc"));
   const [leaderboard, setLeaderboard] = useState<DocumentData[] | undefined>([]);
   const [highScore, setHighScore] = useState(0);
 
-  // const highscoreRef = doc(refUpload, "leaderboard", doc.id);
-  // await updateDoc(highScore, {
-  //   highScore: score
-  // });
 
   const username = "@" + user.data?.email!.slice(0, user.data?.email!.lastIndexOf("@"));
 
-
-
   useEffect(() => {
-    if (!downloadQuery.data?.empty) {
-      const tmp = downloadQuery.data?.docs.map((v) => v.data());
-
-      setLeaderboard(tmp);
-
-      if (tmp?.find((v) => v.name === username) && user.data) {
-        setHighScore(tmp?.find((v) => v.name === username)?.highScore)
+    const updateLeaderboard = onSnapshot(refDownload, { includeMetadataChanges: true },
+      (QuerySnapshot) => {
+        const tmp = QuerySnapshot.docs.map((v) => v.data());
+        setLeaderboard(tmp);
+        const hasHighscore = tmp?.find((v) => v.name === username);
+        if (hasHighscore && user.data && hasHighscore?.highScore > score) {
+          setHighScore(tmp?.find((v) => v.name === username)?.highScore)
+        }
       }
-    }
-  }, [downloadQuery.data, user.data, highScore])
+    );
+    return updateLeaderboard;
+  }, [highScore, started, user.data])
 
 
+  const ref = doc(firestore, "leaderboard", user.data ? user.data?.uid : 'undefined');
+
+  const product = useFirestoreDocument(["leaderboard", user.data ? user.data?.uid : 'undefined'], ref);
+  useEffect(() => {
+    if (product.data?.data()?.highScore)
+      setHighScore(product.data?.data()?.highScore)
+  }, [])
 
 
   const delayResend = 180;
@@ -89,20 +92,22 @@ function App() {
   const seconds = Math.floor(delay % 60);
 
   useEffect(() => {//works
-    const timer = setInterval(() => {
-      setDelay(delay - 1);
-    }, 1000);
+    if (started === true) {
+      const timer = setInterval(() => {
+        setDelay(delay - 1);
+      }, 1000);
 
-    if (delay === 0) {
-      clearInterval(timer);
+      if (delay === 0) {
+        clearInterval(timer);
+      }
+
+      if (minutes === 0 && seconds === 0) { setFail(true); setStarted(false) }
+
+
+      return () => {
+        clearInterval(timer);
+      };
     }
-
-    if (minutes === 0 && seconds === 0) { setFail(true) }
-
-
-    return () => {
-      clearInterval(timer);
-    };
   });
 
   useEffect(() => {//works
@@ -129,7 +134,7 @@ function App() {
           id: user.data?.uid
         })
       }
-
+      setStarted(true);
     }
   }, [score])
 
@@ -163,7 +168,7 @@ function App() {
         setSup(false);
         setWin(false);
       }
-      if (trysLeft - 1 === 0) setFail(true);
+      if (trysLeft - 1 === 0) { setFail(true); setStarted(false) };
       setResult('Type your guess');
     }
     else if (value === 'CLEAR') setResult('Type your guess')
@@ -177,17 +182,15 @@ function App() {
 
 
   return (
-    <div className=" bg-teal-100  h-screen  w-screen">
+    <div className="overflow-hidden bg-teal-100">
       {fail && <div className="">
         <img src={gameOver} alt="Game Over" className="w-screen h-screen absolute" />
         <nav className="absolute flex justify-between inset-x-0 bottom-0 p-[10px]">
-          {/* absolute bg-teal-100 m-auto inset-0 w-[50px] h-[30px] rounded-lg */}
           <input type="button" value="Restart" className="cursor-pointer bg-teal-500 text-white text-xl p-[10px] font-bold rounded-lg w-[100px]" onClick={() => window.location.reload()} />
           <input type="button" value="Quit" className="cursor-pointer bg-teal-500 text-white text-xl p-[10px] font-bold rounded-lg w-[100px]" onClick={() => window.close()} />
         </nav>
       </div>}
-      {/* {win && <img src={youWin} alt="You Win" className="w-screen h-screen" />} */}
-      {!fail && <div className="p-[30px]  h-screen  w-screen">
+      {!fail && <div className="p-[30px] relative">
 
         <h1 className='text-3xl font-bold text-teal-700 flex justify-center mb-[50px]'>Number Guess</h1>
         {username != '@undefined' && <span className="absolute top-5 right-[200px] text-teal-700 text-xl font-bold">{username}</span>}
@@ -199,7 +202,7 @@ function App() {
         {user.data && <input type="button" value="Logout" className="cursor-pointer bg-teal-500 text-xl font-bold text-white absolute top-3 right-3 px-[20px] py-2 flex items-center rounded-[30px] " onClick={() => logOut.mutate()} />}
 
 
-        {<div className={`responsive_SignupPage absolute right-0 top-0 bg-teal-500 w-[350px] ${transition} transition  duration-1000 h-screen`}>
+        {<div className={`absolute right-0 top-0 bg-teal-500 w-[350px] ${transition} transition  duration-500 h-screen`}>
           {closeBtnVisible && <button title="closeBtn" className="absolute top-6 right-6 text-2xl text-white" onClick={() => {
             setTransition('translate-x-full');
             setCloseBtnVisible(false);
@@ -209,6 +212,12 @@ function App() {
             <SignUpGoogle />
           </div>
         </div>}
+        <div className="absolute inset-x-0 top-30 flex justify-center">
+
+          {!started && <input type="button" value='Start' className="cursor-pointer flex items-center justify-center text-3xl font-medium text-white rounded-lg bg-green-600 w-[100px] h-[50px]"
+            onClick={() => setStarted(true)} />}
+
+        </div>
         <div className="mb-[20px] text-teal-700 font-bold">
           <div >Score: {score}</div>
           <div >Highscore: {highScore}</div>
@@ -220,7 +229,6 @@ function App() {
           <div>
 
             <div className="flex items-center justify-center text-3xl font-medium text-white rounded-full bg-red-600 w-[100px] h-[100px]">
-              {/* <Timer /> */}
               <span >
                 {minutes}:{seconds < 10 ? '0' + seconds : seconds}
               </span>
@@ -244,7 +252,7 @@ function App() {
             <div className="grid grid-cols-3 gap-4 w-[300px] mx-auto mt-[70px]">
               {
                 digits.map((value, i) => (
-                  <input key={i} type="button" className="cursor-pointer bg-teal-500 text-white text-xl h-[90px] p-[10px]  font-bold rounded-full" value={value} onClick={() => onSelect(value)} />
+                  <input key={i} type="button" disabled={!started} className={inputStyle({ started: started })} value={value} onClick={() => onSelect(value)} />
                 ))
               }
             </div>
@@ -254,20 +262,34 @@ function App() {
           </div>
         </div>
         <h1 className='text-2xl font-bold text-teal-700 flex justify-center mt-[100px]'>Leaderboard</h1>
-        <div>
-          {
-            leaderboard?.map((value, i) => (
-              <div key={i} className='flex flex-row ml-[40px] justify-center items-center mt-10'>
-                <p className="font-bold mr-3">{value.name}</p>
-                <p className=" ">{value.highScore} points</p>
-              </div>
+        <div className="flex justify-center mt-10 bg-teal-100">
 
-            ))
-          }
+          <table className="">
+            <thead  >
+              <tr>
+                <th className="pb-4">Position</th>
+                <th className="pb-4">User</th>
+                <th className="pb-4">Highscore</th>
+              </tr>
+            </thead>
+
+            {leaderboard?.map((value, i) => (
+              <tbody key={i} className="mt-[50px]">
+                <tr>
+                  <td>#{i + 1}</td>
+                  <td className="px-4">{value.name}</td>
+                  <td>{value.highScore}</td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+
         </div>
       </div>}
     </div>
   );
 }
-
+const inputStyle = ({ started }: { started: boolean }) => classNames('cursor-pointer bg-teal-500 text-white text-xl h-[90px] p-[10px]  font-bold rounded-full', {
+  "bg-teal-800": !started
+})
 export default App;
